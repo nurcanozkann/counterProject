83d1d630d2a86e145d7a21178b8028129ab69aa4
Nurcan Özkan <nurcnozkn3@gmail.com>
2018-12-20 10:13:31 +0300

    asdsadas

--- app/src/main/java/com/example/nurcanzkan/sayac/MainActivity.java
+++ app/src/main/java/com/example/nurcanzkan/sayac/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.nurcanzkan.sayac;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Vibrator;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,12 +14,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     int count=0;
     Button btnTikla;
     SharedPreferences preferences;
+    SharedPreferences ayarlar;
+    LinearLayout linearL;
+    Boolean ses_durumu,titresim_durumu;
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -50,7 +60,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnTikla =findViewById(R.id.btnTikla);
+        linearL=findViewById(R.id.linearL);
         preferences=PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        ayarlar = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        ayarlariGoster();
+        final MediaPlayer ses = MediaPlayer.create(getApplicationContext(),R.raw.tussesi);
+        final Vibrator titresim= (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        //final AudioManager titresim= (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
         count=preferences.getInt("count_anahtarı",0);
         btnTikla.setText(""+count);
@@ -58,13 +74,43 @@ public class MainActivity extends AppCompatActivity {
         btnTikla.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                if(ses_durumu){ses.start();}
+                if(titresim_durumu){titresim.vibrate(500);}
                 count++;
                 btnTikla.setText(""+count);
-
             }
         });
 
+    }
+    private void ayarlariGoster() {
+        String colors=ayarlar.getString("arkaplan","3");
+        switch (Integer.valueOf(colors)){
+            case 0:
+                linearL.setBackgroundColor(Color.RED);
+                break;
+            case 1:
+                linearL.setBackgroundColor(Color.GREEN);
+                break;
+            case 2:
+                linearL.setBackgroundColor(Color.BLUE);
+                break;
+            case 3:
+                linearL.setBackgroundColor(Color.DKGRAY);
+                break;
+            case 4:
+                linearL.setBackgroundColor(Color.LTGRAY);
+                break;
+        }
+        ses_durumu=ayarlar.getBoolean("ses",false);
+        titresim_durumu=ayarlar.getBoolean("titresim",false);
+        ayarlar.registerOnSharedPreferenceChangeListener(MainActivity.this);
 
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        ayarlariGoster();
     }
 }
 
